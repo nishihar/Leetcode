965d9554bb20e750cbfd43401f27e55995ef34e9
Nishihar <nishihar2528@gmail.com>
2026-07-21 21:07:22 +0530

    Time: 1 ms (99%), Space: 42.8 MB (15.06%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -4,13 +4,13 @@ class Solution {
             return false;
         }
            while(n%2==0){
-            n=n/2;
+            n/=2;
         }
          while(n%3==0){
-            n=n/3;
+            n/=3;
         }
          while(n%5==0){
-            n=n/5;
+            n/=5;
         }
        
         return n==1;
